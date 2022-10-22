6cff32c9f4a2f7d684592fc68fda6cfaff70f09e
U-AD\pogoreli <pogoreli@ECE419-6V259R3.ad.lafayette.edu>
2022-10-22 15:36:16 -0400

    Added pydoc comments

--- hangman_library/HangmanLibrary.py
+++ hangman_library/HangmanLibrary.py
@@ -1,5 +1,6 @@
 import random
 
+'''This class is designed as backend to GUI and TUI versions of Hangman game'''
 class HangmanLibrary:
     words = []
     lettersAlreadyUsed = []
@@ -7,24 +8,29 @@ class HangmanLibrary:
     score = 0
     word = ""
 
+    '''The library imports the words from the file on startup'''
     def __init__(self):
         self.importWords()
 
+    '''A function for word import. Removes short lines (less than 2 symbols) and removes the new line character'''
     def importWords(self):
         with open('words.txt') as file:
             for line in file:
                 if len(line) >= 2:
                     self.words.append(line.rstrip())
 
+    '''Function called before each game. Resets score, mistakes count, and letters already used. Picks a new word for the game'''
     def startNewGame(self):
         self.mistakesCount = 0
         self.score = 0
         self.lettersAlreadyUsed = []
         self.word = self.pickRandomWord()
 
+    '''Picks a word from the list'''
     def pickRandomWord(self):
         return random.choice(self.words)
 
+    '''Receives a new letter from a user and checks wheter it was already used and whether it exists in the word'''
     def submitNewLetter(self, letter):
         if not self.checkIfLetterAlreadyUsed(letter):
             if self.checkIfLetterExists():
@@ -34,12 +40,14 @@ class HangmanLibrary:
         else:
             print("This letter has already been used")  #Need to add something to inform user or throw an error
 
+    '''Checks if letter exists in the word. Returns True if exists in the word'''
     def checkIfLetterExists(self, letter) -> bool:
         for symbol in self.word:
             if symbol == letter:
                 return True
         return False
 
+    '''Checks whether letter was used in this round. Returns True if already used'''
     def checkIfLetterAlreadyUsed(self, letter) -> bool:
         for symbol in self.lettersAlreadyUsed:
             if symbol == letter:
--- hangman_library/UnitTests.py
+++ hangman_library/UnitTests.py
@@ -1,5 +1,14 @@
 import unittest
 
+# Method	                 Equivalent to
+# .assertEqual(a, b)	     a == b
+# .assertTrue(x)	         bool(x) is True
+# .assertFalse(x)	         bool(x) is False
+# .assertIs(a, b)	         a is b
+# .assertIsNone(x)	         x is None
+# .assertIn(a, b)	         a in b
+# .assertIsInstance(a, b)	 isinstance(a, b)
+
 class TestHangmanLibrary(unittest.TestCase):
     def testOne(self):
         self.assertTrue(False)
