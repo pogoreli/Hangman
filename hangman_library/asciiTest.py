6c804d434f7e8b94c7d6ca8b605897bd18b8ee90
U-AD\pogoreli <pogoreli@ECE419-6V259R3.ad.lafayette.edu>
2022-10-23 23:10:15 -0400

    ASCII magic

---
+++ hangman_library/asciiTest.py
@@ -0,0 +1,7 @@
+for i in range(97, 123):
+    letter = chr(i)
+    print("ASCII code: " + str(i) + "; Letter: " + str(letter) + ";")
+
+for i in range(65, 91):
+    letter = chr(i)
+    print("ASCII code: " + str(i) + "; Letter: " + str(letter) + ";")
